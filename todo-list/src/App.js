f4b2258b505130453b43e2636e11a4efc011c228
Arlo Wotherspoon <arlowotherspoon@gmail.com>
2021-08-06 01:47:11 -0700

    added to todo-list

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [inputValue, setInputValue] = useState('')
+
+  const list = []
+
+
+
+  function handleChange(event) {
+    setInputValue(event.target.value)
+    
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    list.push(inputValue)
+    setInputValue('')
+    console.log(list)
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <div className="formWrapper">
+      <h1> To-do List </h1>
+      <form onSubmit={ handleSubmit }> 
+        <input value={ inputValue } onChange={ handleChange } onSubmit={ handleSubmit }/> 
+      </form>
+      <div className="list">
+        { list.map(item => {
+
+          console.log(item)
+
+          return (
+            <h4>
+              {item}
+            </h4>
+          )
+        })}
+      </div>
+  </div>);
 }
 
 export default App;
+
+// Title
+// Sub-title
+// List
+// User Input
